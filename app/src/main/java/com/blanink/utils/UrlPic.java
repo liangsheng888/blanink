e8323521f42bae008b4247f82f5ceb00c9668f06
liangliang <978188219@qq.com>
2017-07-28 11:32:19 +0800

    LIANGLAING

--- app/src/main/java/com/blanink/utils/UrlPic.java
+++ app/src/main/java/com/blanink/utils/UrlPic.java
@@ -4,7 +4,9 @@ package com.blanink.utils;
  * Created by Administrator on 2017/7/27 0027.
  */
 public class UrlPic {
-    public static String pic="http://b.hiphotos.baidu.com/image/pic/item/d01373f082025aaf95bdf7e4f8edab64034f1a15.jpg";
-    public static String pic2="http://g.hiphotos.baidu.com/image/pic/item/6159252dd42a2834da6660c459b5c9ea14cebf39.jpg";
-    public static String pic3="http://d.hiphotos.baidu.com/image/pic/item/adaf2edda3cc7cd976427f6c3901213fb80e911c.jpg";
+    public static String pic="http://pic30.nipic.com/20130626/8174275_085522448172_2.jpg";
+    public static String pic2="http://pic18.nipic.com/20111215/577405_080531548148_2.jpg";
+    public static String pic3="http://pic15.nipic.com/20110722/2912365_092519919000_2.jpg";
+    public static String pic4="http://h.hiphotos.baidu.com/image/pic/item/faedab64034f78f0b00507c97e310a55b3191cf9.jpg";
+
 }
